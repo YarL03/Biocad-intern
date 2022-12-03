b8210ad0c18876766c3c2988cd9adbd4a16f56bf
YarL03 <93777631+YarL03@users.noreply.github.com>
2022-12-03 14:17:13 +0800

    refactor: deleted logs

--- public/analytics.js
+++ public/analytics.js
@@ -5,13 +5,11 @@ const like = document.querySelector('.like')
 fetch('https://biocad-task.onrender.com/api/analytics?liked')
 .then(res => res.json())
 .then(data => {
-    console.log(data)
     if (data) like.children[0].children[0].setAttribute('fill', '#EE3F44')
 
     like.setAttribute('data-liked', data)
 
     like.addEventListener('click', (e) => {
-        console.log(like.getAttribute('data-liked'))
         const liked = like.getAttribute('data-liked') === 'true' ? false : true
 
         liked ? like.children[0].children[0].setAttribute('fill', '#EE3F44')
